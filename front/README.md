e2d6cb4a5ae99e740e96f0530bde0a0576ae6246
Daniil <daniil.murashev@gmail.com>
2021-11-03 09:36:45 +0000

    Start

--- front/README.md
+++
@@ -1,1 +0,0 @@
-# JustCamp Next
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,29 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withOptimizedImages = require("next-optimized-images");
+const withFonts = require("next-fonts");
+const path = require('path')
+
+
+require('dotenv').config()
+
+module.exports = withFonts(
+  withOptimizedImages({
+
+      env: {
+          API_URL: process.env.API_URL,
+          TOKEN_SECRET: process.env.TOKEN_SECRET
+      },
+      publicRuntimeConfig: {
+          // Will be available on both server and client
+          API_URL: process.env.API_URL,
+          TOKEN_SECRET: process.env.TOKEN_SECRET,
+      },
+      webpack: config => {
+          config.resolve.alias['components'] = path.join(__dirname, 'components')
+          config.resolve.alias['public'] = path.join(__dirname, 'public')
+
+          return config
+      }
+    /* config for next-optimized-images */
+    // your config for other plugins or the general next.js here...
+  })
+);
